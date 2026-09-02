a2c7e1f8e91996156fe4be5069996893aa9084b9
wangzheren <wangzheren@corp.netease.com>
2026-09-02 17:51:48 +0800

    test(k380): restore ws2812 render with matrix delay

--- zmk-keyboard-k380/src/status_indicator.c
+++ zmk-keyboard-k380/src/status_indicator.c
@@ -158,8 +158,11 @@ static int render_status(enum k380_status_id status) {
     k380_status_indicator_test_render(status, status_pixels, ARRAY_SIZE(status_pixels));
     return 0;
 #else
-    ARG_UNUSED(status_led_strip);
-    return 0;
+    if (!device_is_ready(status_led_strip)) {
+        return -ENODEV;
+    }
+
+    return led_strip_update_rgb(status_led_strip, status_pixels, ARRAY_SIZE(status_pixels));
 #endif
 }
 
